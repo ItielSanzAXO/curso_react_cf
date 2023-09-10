77dad72e6193063e2af2e9347264adf578eeec69
ItielSanz /AXO <113627289+ItielSanzAXO@users.noreply.github.com>
2023-09-09 19:40:37 -0600

    Update

---
+++ src/components/Menu.js
@@ -0,0 +1,17 @@
+function Menu() {
+    //Todos los componentes deben devolver un elemento
+  return (
+    <header className="App-header">
+    <nav>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Personajes</a></li>
+        <li><a href="#">Episodios</a></li>
+        <li><a href="#">Citas</a></li>
+      </ul>
+    </nav>
+  </header>
+  );
+}
+
+export default Menu;
